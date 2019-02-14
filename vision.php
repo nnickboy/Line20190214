1fb730f4bf25eee497ec8c838a68c1ae84e6da93
nnickboy <47617977+nnickboy@users.noreply.github.com>
2019-02-14 16:53:44 +0800

    Update vision.php

--- vision.php
+++ vision.php
@@ -13,7 +13,7 @@
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU='
+        'Authorization: Bearer fYSTcNaZ/hxGDOrKXK/dclPnVzHGIctM9jHPoSiW8oDXqI2nr7GnjmLE5ts43Er2QI/xjJl68eiYPG7WG243f0q2GCA8+9G0IzHRG9IpCH5uACf5vMJ2TQ4KOUJhnsgTHEBJRKW1rIaFYI2GpiznKQdB04t89/1O/w1cDnyilFU='
     ));
     $json_content = curl_exec($ch);
     curl_close($ch);
@@ -68,7 +68,7 @@
   
     fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
     $header[] = "Content-Type: application/json";
-    $header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+    $header[] = "Authorization: Bearer fYSTcNaZ/hxGDOrKXK/dclPnVzHGIctM9jHPoSiW8oDXqI2nr7GnjmLE5ts43Er2QI/xjJl68eiYPG7WG243f0q2GCA8+9G0IzHRG9IpCH5uACf5vMJ2TQ4KOUJhnsgTHEBJRKW1rIaFYI2GpiznKQdB04t89/1O/w1cDnyilFU=";
     $ch = curl_init("https://api.line.me/v2/bot/message/reply");
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
